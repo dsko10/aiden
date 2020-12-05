1b7543fd931e42d3560c59a6b403a6da49498375
Damian Skowron <dskowron9@gmail.com>
2020-12-05 12:22:32 +0100

    Init

---
+++ src/main/java/com/example/aiden/AidenApplication.java
@@ -0,0 +1,11 @@
+package com.example.aiden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AidenApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AidenApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/aiden/AidenApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.aiden;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AidenApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
